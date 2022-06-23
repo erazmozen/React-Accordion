192ae4cf147b189ef31104bb494eb76bf0623e0c
собака <erazmozen@protonmail.com>
2022-06-23 18:23:29 +0200

    Added theme and general cleanup

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,52 +1,49 @@
-import React, {useState, useRef} from 'react'
-import "./Accordion.css"
-import Chevron from './Chevron';
+import React, { useState, useRef } from "react";
+import "./Accordion.css";
+import Chevron from "./Chevron";
 
 function Accordion(props) {
+  const [setActive, setActiveState] = useState("");
+  const [setHeight, setHightState] = useState("0px");
+  const [setRotate, setRotateState] = useState("accordion__icon");
 
-    const [setActive, setActiveState] = useState("")
-    const [setHeight, setHightState] = useState("0px")
-    const [setRotate, setRotateState] = useState("accordion__icon")
+  const content = useRef(null);
 
-    const content = useRef(null)
+  function toggleAccordion() {
+    setActiveState(setActive === "" ? "active" : "");
+    setHightState(
+      setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
+    );
+    setRotateState(
+      setActive === "active" ? "accordion__icon" : "accordion__icon rotate"
+    );
+  }
 
-    function toggleAccordion() {
-        setActiveState(setActive === "" ? "active" : "")
-        setHightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`)
-        setRotateState(setActive === "active" ? "accordion__icon" : "accordion__icon rotate")
-    }
+  return (
+    <div className="accordion__section">
+      <button
+        className={`accordion ${setActive} ${
+          setActive === "active" ? "" : "border__round"
+        }`}
+        onClick={toggleAccordion}
+      >
+        <p className="accordion__title">{props.title}</p>
 
-    return (
+        <Chevron className={`${setRotate}`} width={10} fill={"#777"} />
+      </button>
+
+      <div
+        ref={content}
+        style={{ maxHeight: `${setHeight}` }}
+        className="accordion__content"
+      >
         <div
-        className='accordion__section'>
-            <button
-                className={`accordion ${setActive}`}
-                onClick={toggleAccordion}>
-
-                    <p
-                        className='accordion__title'>{props.title}
-                    </p>
-
-                    <Chevron
-                        className={
-                            `${setRotate}`
-                        }
-                        width={10}
-                        fill={"#777"}
-                    />
-            </button>
-
-            <div
-                ref={content}
-                style={{maxHeight: `${setHeight}`}}
-                className='accordion__content'>
-
-                <div
-                    className='accordion__text'
-                    dangerouslySetInnerHTML={{ __html: props.content }}/>
-            </div>
-        </div>
-    )
+          className="accordion__text"
+          dangerouslySetInnerHTML={{ __html: props.content }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Accordion;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Accordion from './components/Accordion';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Accordion from "./components/Accordion";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Accordion
-    title="What is Lorem Ipsum?"
-    content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-    />
-    <Accordion
-    title="What is Lorem Ipsum?"
-    content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-    />
-    <Accordion
-    title="What is Lorem Ipsum?"
-    content="
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <div className="wrapper">
+      <Accordion
+        title="What is Lorem Ipsum?"
+        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+      />
+      <Accordion
+        title="Why do we use it?"
+        content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
+      />
+      <Accordion
+        title="Where does it come from??"
+        content="
+    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock.</p>
     </br>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <p>From a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
     </br>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <p>Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
     "
-    />
+      />
+    </div>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
